a9392c520ed879deda9eec3dad82559f9a971913
pldrake26 <pldrake26@outlook.com>
2017-06-14 02:58:30 -0500

    Removed default react header and emptied start up state.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="col-sm-6">
+            <ShoppingList />
         </div>
-        <ShoppingList />
+        <div className="col-sm-6">
+
+        </div>
       </div>
     );
   }
--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -4,7 +4,8 @@ export default class GroceryList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            listItems: ['Banana', 'Apple', 'Orange']
+            listItems: [],
+            message: 'There are no items on your list.'
         }
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -18,6 +19,7 @@ export default class GroceryList extends Component {
                 newItem
             ]
         });
+        document.getElementById('data').value='';
     }
     render() {
         return (
@@ -26,13 +28,13 @@ export default class GroceryList extends Component {
                 <hr/>
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="grocery-item">Grocery Item</label>
-                        <input type="text" className="form-control" id="data"/>
+                        <label htmlFor="grocery-item">Add grocery item below:</label>
+                        <input type="text" className="form-control" id="data" placeholder="Add item here..."/>
                     </div>
-                    <input type="submit" value="Submit"/>
+                    <input type="submit" value="Add Item"/>
                 </form>
-                <br/>
-                <div className="display text-left">
+                <hr/>
+                <div className="display">
                     {
                         this.state.listItems.map((item, index) => {
                             return <p style={{paddingLeft: 3 + '%'}} key={index}>{item}</p>
