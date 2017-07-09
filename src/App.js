5dd50c9519df9f0fa3657a2405fa757d3d014555
pldrake26 <pldrake26@outlook.com>
2017-07-09 01:54:28 -0500

    Added styled-components package.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import styled from 'styled-components';
 import './App.css';
 import ShoppingList from './ShoppingList.js';
 import PrintList from './PrintList.js';
 
+const AppBackground = styled.div`
+    background-color: pink;
+`
+
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <div className="col-sm-6">
-            <Router>
-                <div>
-                    <Link to="/"><button type="button" className="btn btn-default"><span className="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>&nbsp;&nbsp;Shopping List</button></Link>&nbsp;&nbsp;
-                    <Link to="/print"><button type="button" className="btn btn-default"><span className="glyphicon glyphicon-print" aria-hidden="true"></span>&nbsp;&nbsp;Print</button></Link>
-                    <Route exact path="/" component={ShoppingList} />
-                    <Route path="/print" component={PrintList} />
-                </div>
-            </Router>
-        </div>
-        <div className="col-sm-6">
+        <div className="app">
+            <div className="col-sm-6">
+                <Router>
+                    <div>
+                        <Link to="/"><button type="button" className="btn btn-default"><span className="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>&nbsp;&nbsp;Shopping List</button></Link>&nbsp;&nbsp;
+                        <Link to="/print"><button type="button" className="btn btn-default"><span className="glyphicon glyphicon-print" aria-hidden="true"></span>&nbsp;&nbsp;Print</button></Link>
+                        <Route exact path="/" component={ShoppingList} />
+                        <Route path="/print" component={PrintList} />
+                    </div>
+                </Router>
+            </div>
+            <div className="col-sm-6">
 
+            </div>
         </div>
-      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
